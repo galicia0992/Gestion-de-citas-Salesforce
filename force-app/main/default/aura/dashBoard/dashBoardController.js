034668f06d3b76e53f58f4610904e9c316b42369
galicia0992 <perrako92@live.com.mx>
2024-09-21 20:27:24 -0600

    Feature: se crea boton y modal para alta de citas, querda funcional

--- force-app/main/default/aura/dashBoard/dashBoardController.js
+++ force-app/main/default/aura/dashBoard/dashBoardController.js
@@ -1,24 +1,35 @@
 ({
-    openModalDoc : function(component, event, helper) {
+    openModalDoc: function (component, event, helper) {
         let open = component.get('v.openModalDoc')
-        
-       
-        if(open == false){
+
+
+        if (open == false) {
             component.set('v.openModalDoc', true)
-        }else{
+        } else {
             component.set('v.openModalDoc', false)
         }
-        
+
     },
-    openModalPaciente : function(component, event, helper) {
+    openModalPaciente: function (component, event, helper) {
         let open = component.get('v.openModalPaciente')
-        
-       
-        if(open == false){
+
+
+        if (open == false) {
             component.set('v.openModalPaciente', true)
-        }else{
+        } else {
             component.set('v.openModalPaciente', false)
         }
-        
+
+    },
+    openModalCita: function (component, event, helper) {
+        let open = component.get('v.openModalCitas')
+
+
+        if (open == false) {
+            component.set('v.openModalCitas', true)
+        } else {
+            component.set('v.openModalCitas', false)
+        }
+
     }
 })
---
+++ force-app/main/default/aura/modalAltaCitas/modalAltaCitasController.js
@@ -0,0 +1,17 @@
+({
+    myAction : function(component, event, helper) {
+
+    },
+    handleSuccess : function(component, event, helper) {
+        var toastEvent = $A.get("e.force:showToast");
+        toastEvent.setParams({
+            "title": "Success!",
+            "message": "The record " + + event.getParam("id") + " has been created successfully."
+        });
+        toastEvent.fire();
+        component.set('v.openModalCitas', false)
+    },
+    closeModal : function (component) {
+        component.set('v.openModalCitas', false)
+    }
+})
